f082afc7fc6c23cfee5ca1a089df721827c15ba0
Shruti Gupta <113260340+shrutigupta263@users.noreply.github.com>
2024-06-03 18:47:52 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,16 @@
+"use strict";//all should be in new version based only//
+//alert(3+3) here no operation perform becuse of using nodejs its only in console//
+console.log(3+3)
+console.log("shruti")
+
+
+let name = "shruti"
+let age = 20
+let isLoggedIn = false
+let state;
+ console.log(typeof undefined);
+ console.log(typeof null);
+ console.log(typeof shruti);
+ console.log(typeof age); 
+ console.log(typeof state); 
+ 
